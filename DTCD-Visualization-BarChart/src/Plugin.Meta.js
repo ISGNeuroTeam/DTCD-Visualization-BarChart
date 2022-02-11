4c5036e6785f54106ef498d0fe472dea1a43a373
sbelikov98 <sbelikov@isgneuro.com>
2022-02-11 16:54:36 +0300

    Added version for core systems, changed build process

--- DTCD-Visualization-BarChart/src/Plugin.Meta.js
+++ DTCD-Visualization-BarChart/src/Plugin.Meta.js
@@ -1,7 +1,9 @@
+import { version } from './../package.json';
+
 export default {
+  version,
   type: 'panel',
   name: 'Visualization_BarChart',
   title: 'Визуализация - Столбчатая диаграмма',
-  version: '0.1.0',
   withDependencies: true,
 };
